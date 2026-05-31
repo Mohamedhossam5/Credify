d9a4698367d2dd4698415fd0637ef3d7e9e43bbe
Mahmoud-Nady8 <Mahmoud.Nady.22231077@commerce.helwan.edu.eg>
2026-05-31 15:40:27 +0300

    show notification tooltip on hover for better UX

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Trigger notification bell shortcut & premium tooltip
+import('../scratch/add_notification_shortcut.js').catch(console.error);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
